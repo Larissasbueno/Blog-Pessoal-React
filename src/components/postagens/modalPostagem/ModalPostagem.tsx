93b8f23b7ca41720a1af155a6a52eb86bd42d7ac
Larissasbuenno <larissasbuenno@gmail.com>
2023-08-02 09:11:15 -0300

    info: Atualização Modal e instalação popup

--- src/components/postagens/modalPostagem/ModalPostagem.tsx
+++ src/components/postagens/modalPostagem/ModalPostagem.tsx
@@ -1,65 +1,22 @@
-import React, { useContext, useEffect, useState } from 'react';
-import { Dna } from 'react-loader-spinner';
-import { useNavigate } from 'react-router-dom';
-import { AuthContext } from '../../../contexts/AuthContext';
-import Postagem from '../../../models/Postagem';
-import { buscar } from '../../../services/Service';
-import CardPostagem from '../cardPostagem/CardPostagem';
-import { toastAlerta } from '../../../util/toastAlerta';
+import React from 'react';
+import FormularioPostagem from '../formularioPostagem/FormularioPostagem';
 
-function ListaPostagens() {
-  const [postagens, setPostagens] = useState<Postagem[]>([]);
+import 'reactjs-popup/dist/index.css';
+import Popup from 'reactjs-popup';
 
-  let navigate = useNavigate();
+import './ModalPostagem.css'
 
-  const { usuario, handleLogout } = useContext(AuthContext);
-  const token = usuario.token;
-
-  useEffect(() => {
-    if (token === '') {
-      toastAlerta('Você precisa estar logado', 'info');
-      navigate('/');
-    }
-  }, [token]);
-
-  async function buscarPostagens() {
-    try {
-      await buscar('/postagens', setPostagens, {
-        headers: {
-          Authorization: token,
-        },
-      });
-    } catch (error: any) {
-      if (error.toString().includes('403')) {
-        toastAlerta('O token expirou, favor logar novamente', 'info')
-        handleLogout()
-      }
-    }
-  }
-
-  useEffect(() => {
-    buscarPostagens();
-  }, [postagens.length]);
-
+function ModalPostagem() {
   return (
     <>
-      {postagens.length === 0 && (
-        <Dna
-          visible={true}
-          height="200"
-          width="200"
-          ariaLabel="dna-loading"
-          wrapperStyle={{}}
-          wrapperClass="dna-wrapper mx-auto"
-        />
-      )}
-      <div className='container mx-auto my-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'>
-        {postagens.map((postagem) => (
-          <CardPostagem key={postagem.id} post={postagem} />
-        ))}
-      </div>
+      <Popup 
+      trigger={<button className='border rounded px-4 hover:bg-white hover:text-indigo-800'>Nova postagem</button>} modal>
+        <div>
+          <FormularioPostagem />
+        </div>
+      </Popup>
     </>
   );
 }
 
-export default ListaPostagens;
+export default ModalPostagem;
